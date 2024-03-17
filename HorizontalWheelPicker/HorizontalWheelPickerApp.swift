642ef271e7738fb6314f12b8be98f90a23616285
HorevyiNazar <goreviynazar@gmail.com>
2024-03-17 11:38:23 +0100

    Initial Commit

---
+++ HorizontalWheelPicker/HorizontalWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalWheelPickerApp.swift
+//  HorizontalWheelPicker
+//
+//  Created by Назар Горевой on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
