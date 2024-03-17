aa50a77441c566a7937add70b5aa01aa3e473be8
HorevyiNazar <goreviynazar@gmail.com>
2024-03-17 12:25:41 +0100

    Horizontal Wheel Picker

--- HorizontalWheelPicker/ContentView.swift
+++ HorizontalWheelPicker/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var config: WheelPicker.Config = .init(count: 30, steps: 5, spacing: 15, multiplier: 10)
+    @State private var value: CGFloat = 10
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .lastTextBaseline, spacing: 5, content: {
+                    Text(verbatim: "\(value)")
+                        .font(.largeTitle.bold())
+                        .contentTransition(.numericText(value: value))
+                        .animation(.snappy, value: value)
+                    
+                    Text("lbs")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .textScale(.secondary)
+                        .foregroundStyle(.gray)
+                    
+
+                })
+                .padding(.bottom, 30)
+                
+                WheelPicker(config: config, value: $value)
+                    .frame(height: 60)
+            }
+            .navigationTitle("Wheel Picker")
         }
-        .padding()
     }
 }
 
